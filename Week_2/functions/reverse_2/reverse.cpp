696229fbd9b403716351430f45f39cf0e08bb331
GAlekseyV <galekseyv@gmail.com>
2021-11-04 09:26:24 +0300

    Добавь задачи и конспект части 1 недели 2.

---
+++ Week_2/functions/reverse_2/reverse.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> Reversed(const vector<int>& input)
+{
+    vector<int> result;
+    for (int i = input.size() - 1; i >= 0; --i) {
+        result.push_back(input[i]);
+    }
+    return result;
+}

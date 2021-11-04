91b7cf0a93f1c364ea8fe61afc7e1f108198237c
GAlekseyV <galekseyv@gmail.com>
2021-11-04 09:26:24 +0300

    Добавь задачи и конспект части 1 недели 2.

---
+++ Week_2/functions/isPalindrom/ispalindrom.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+bool IsPalindrom(string s) {
+  // Замечание: более правильным было бы использовать здесь тип size_t вместо
+  // int О причинах Вы узнаете на Жёлтом поясе
+  for (int i = 0; i < s.size() / 2; ++i) {
+    if (s[i] != s[s.size() - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
